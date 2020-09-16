59173dcc0f9a937f3f3a77665ef88643a59548e7
Rufino John <aguilarufino@gmail.com>
2020-09-16 08:35:50 +0800

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,28 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'dashboard',
     pathMatch: 'full'
   },
+  // {
+  //   path: 'folder/:id',
+  //   loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  // },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'dashboard',
+    loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  },
+  {
+    path: 'maps',
+    loadChildren: () => import('./pages/maps/maps.module').then( m => m.MapsPageModule)
+  },
+  {
+    path: 'advisories',
+    loadChildren: () => import('./pages/advisories/advisories.module').then( m => m.AdvisoriesPageModule)
+  },
+  {
+    path: 'cases',
+    loadChildren: () => import('./pages/cases/cases.module').then( m => m.CasesPageModule)
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,37 +13,27 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'Dashboard',
+      url: 'dashboard',
+      icon: 'bar-chart'
     },
     {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
+      title: 'Maps',
+      url: 'maps',
+      icon: 'map'
     },
     {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
+      title: 'Advisories',
+      url: 'advisories',
       icon: 'warning'
+    },
+    {
+      title: 'Cases',
+      url: 'cases',
+      icon: 'people'
     }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public agencies = ['PGB', 'PNP', 'DOH', 'PICTD'];
 
   constructor(
     private platform: Platform,
