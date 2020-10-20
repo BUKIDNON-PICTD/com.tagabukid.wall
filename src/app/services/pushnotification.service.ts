3be4aa2db2d459c105906723c5c06c1c37ad5dbe
Rufino John <aguilarufino@gmail.com>
2020-10-20 08:29:18 +0800

    added angular push notification

---
+++ src/app/services/pushnotification.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PushnotificationService {
+
+  constructor(private http: HttpClient) { }
+  
+  public sendSubscriptionToTheServer(subscription: PushSubscription) {
+    return this.http.post(`${environment.panganud}/api/covid19subscriber/subscribe`, subscription);
+  }
+}
