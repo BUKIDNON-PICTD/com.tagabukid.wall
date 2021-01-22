2f3a228b303723011b232b4d039901f890a03c92
hoycid <quilangcidrex@gmail.com>
2021-01-22 09:27:28 +0800

    added vaccine survey dashboard UI

--- src/app/pages/vaccinesurveydashboard/vaccinesurveydashboard.page.ts
+++ src/app/pages/vaccinesurveydashboard/vaccinesurveydashboard.page.ts
@@ -6,10 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vaccinesurveydashboard.page.scss'],
 })
 export class VaccinesurveydashboardPage implements OnInit {
+  public currentDateTime;
+
+  public totalMalesYes: number;
+  public totalMalesNo: number;
+  public totalMalesUndecided: number;
+  public totalMales: number;
+  public totalRespondents: number;
+
+  public totalFemalesYes: number;
+  public totalFemalesNo: number;
+  public totalFemalesUndecided: number;
+  public totalFemales: number;
 
   constructor() { }
 
   ngOnInit() {
+    this.currentDateTime = new Date().toLocaleString();
+
+    this.totalMalesYes = 2;
+    this.totalMalesNo = 21;
+    this.totalMalesUndecided = 19;
+    this.totalMales = this.totalMalesYes + this.totalMalesNo + this.totalMalesUndecided;
+
+    this.totalFemalesYes = 4;
+    this.totalFemalesNo = 17;
+    this.totalFemalesUndecided = 24;
+    this.totalFemales = this.totalFemalesYes + this.totalFemalesNo + this.totalFemalesUndecided;
+
+    this.totalRespondents = 87;
   }
 
 }
---
+++ src/app/services/vaccinesurveydashboardservice.service.ts
@@ -0,0 +1,100 @@
+import { AlertController } from "@ionic/angular";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, tap } from "rxjs/operators";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VaccinesurveydashboardserviceService {
+
+  constructor(
+    public http: HttpClient,
+    public alertController: AlertController
+  ) {}
+
+  getItems(): Promise<any[]> {
+    return this.http
+      .get<any>(`${environment.panganud}/api/vaccinesurveydashboard`)
+      .pipe(
+        tap((res) => {
+          return res;
+        }),
+        catchError((e) => {
+          this.showAlert(e.error.msg);
+          throw new Error(e);
+        })
+      )
+      .toPromise();
+  }
+
+  getItem(id): Promise<any> {
+    return this.http
+      .get<any>(`${environment.panganud}/api/vaccinesurveydashboard/` + id)
+      .pipe(
+        tap((res) => {
+          return res;
+        }),
+        catchError((e) => {
+          this.showAlert(e.error.msg);
+          throw new Error(e);
+        })
+      )
+      .toPromise();
+  }
+
+  addItem(item: any): Promise<any> {
+    return this.http
+      .post(`${environment.panganud}/api/vaccinesurveydashboard/`, item)
+      .pipe(
+        tap((res) => {
+          return res;
+        }),
+        catchError((e) => {
+          this.showAlert(e.error.msg);
+          throw new Error(e);
+        })
+      )
+      .toPromise();
+  }
+
+  updateItem(item: any): Promise<any> {
+    return this.http
+      .put(`${environment.panganud}/api/vaccinesurveydashboard/` + item.id, item)
+      .pipe(
+        tap((res) => {
+          return res;
+        }),
+        catchError((e) => {
+          this.showAlert(e.error.msg);
+          throw new Error(e);
+        })
+      )
+      .toPromise();
+  }
+
+  deleteItem(id: any): Promise<any> {
+    return this.http
+      .delete(`${environment.panganud}/api/vaccinesurveydashboard/` + id)
+      .pipe(
+        tap((res) => {
+          return res;
+        }),
+        catchError((e) => {
+          this.showAlert(e.error.msg);
+          throw new Error(e);
+        })
+      )
+      .toPromise();
+  }
+
+  showAlert(msg) {
+    let alert = this.alertController.create({
+      message: msg,
+      header: "Error",
+      buttons: ["OK"],
+    });
+    alert.then((alert) => alert.present());
+  }
+}
