85498fa9a4c466007ba505455b44c918afbd6218
bylgrad <bylgrad@gmail.com>
2021-01-27 14:28:26 +0800

    remove duplicate vaxx survey tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -69,11 +69,6 @@ export class AppComponent implements OnInit {
       icon: "medkit",
     },
     {
-      title: "Vaccine Survey Dashboard",
-      url: "vaccinesurveydashboard",
-      icon: "chart",
-    },
-    {
       title: "QR Profile List",
       url: "qrprofilelist",
       icon: "list",
